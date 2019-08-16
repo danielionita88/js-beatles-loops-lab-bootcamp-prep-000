4b014dcb57b56c5ac22c84dfd8f203e662217903
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-08-16 16:56:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var cantareti = [];
+  for (let i = 0; i < musicians.length; i++) {
+    cantareti.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return cantareti;
+}
+
+function johnLennonFacts(arr) {
+  var i = 0;
+  while (i < arr.length){
+    arr.push(arr[i] + "!!!");
+    i++;
+  }
+  return arr;
+}
